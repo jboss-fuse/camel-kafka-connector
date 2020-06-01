a1dc0c90c95ef3cfcbf24d455deaacb043e4a6bd
Otavio Rodolfo Piske <opiske@redhat.com>
2020-06-01 14:02:52 +0200

    Prevent a NPE by creating the config class
    On a few occasions it could be null, thus causing the startup to fail
    and some of the CamelSourceSalesforceITCase to fail

--- connectors/camel-salesforce-kafka-connector/src/main/java/org/apache/camel/component/salesforce/SalesforceComponentCKC.java
+++ connectors/camel-salesforce-kafka-connector/src/main/java/org/apache/camel/component/salesforce/SalesforceComponentCKC.java
@@ -19,16 +19,34 @@ package org.apache.camel.component.salesforce;
 import java.lang.reflect.Field;
 
 import org.apache.camel.spi.PropertyConfigurer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 //XXX: temporary workaround waiting for https://issues.apache.org/jira/browse/CAMEL-15063 in camel 3.4.0
 public class SalesforceComponentCKC extends SalesforceComponent {
+    private static final Logger LOG = LoggerFactory.getLogger(SalesforceComponentCKC.class);
 
     @Override
     protected void doStart() throws Exception {
         super.doStart();
         Field httpClient = this.getClass().getSuperclass().getDeclaredField("httpClient");
-        httpClient.setAccessible(true);
-        getConfig().setHttpClient((SalesforceHttpClient) httpClient.get(this));
+
+        if (httpClient == null) {
+            LOG.warn("The http client field is null on the super class");
+        } else {
+            LOG.debug("Making the http client accessible on the super class");
+            httpClient.setAccessible(true);
+
+            SalesforceEndpointConfig config = getConfig();
+            if (config == null) {
+                LOG.debug("The end point configuration instance is null on the super class");
+                config = new SalesforceEndpointConfig();
+                setConfig(config);
+            }
+
+            LOG.debug("Overwriting the http client instance the super class");
+            config.setHttpClient((SalesforceHttpClient) httpClient.get(this));
+        }
     }
 
     @Override
